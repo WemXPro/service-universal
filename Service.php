73995c3e084312450da32d61358c419a53f1736f
Mubeen142 <mubeenullah323@gmail.com>
2023-10-20 14:23:27 +0200

    Update Service.php

--- Service.php
+++ Service.php
@@ -6,7 +6,7 @@ use App\Models\Order;
 
 class Service
 {
-    protected static $key = 'pterodactyl'; 
+    protected static $key = 'universal'; 
 
     public function __construct(Order $order)
     {
@@ -25,7 +25,7 @@ class Service
           'display_name' => 'Universal',
           'autor' => 'WemX',
           'version' => '1.0.0',
-          'wemx_version' => ['dev', '>=1.8.0'],
+          'wemx_version' => ['*'],
         ];
     }
 
@@ -35,30 +35,11 @@ class Service
      *
      * Laravel validation rules: https://laravel.com/docs/10.x/validation
      *
-     * @return collection
+     * @return array
      */
-    public static function setConfig()
+    public static function setConfig(): array
     {
-        return collect([
-            "encrypted::pterodactyl::api_url" => [
-                "name" => "Host",
-                "description" => "The hostname of your Pterodactyl Panel i.e panel.example.com",
-                "type" => "text", // text, textarea, password, number, date, checkbox, url, email, select
-                "rules" => ['required'], // laravel validation rules
-            ],
-            "encrypted::pterodactyl::api_key" => [
-                "name" => "API Key",
-                "description" => "The API key for your Pterodactyl Panel",
-                "type" => "password",
-                "rules" => ['required'], // laravel validation rules
-            ],
-            "encrypted::pterodactyl::sso_secret" => [
-                "name" => "SSO Secret Key",
-                "description" => "The SSO key used for automating logging in to Pterodactyl Panel",
-                "type" => "password",
-                "rules" => ['nullable'], // laravel validation rules
-            ]
-        ]);
+        return [];
     }
 
     /**
@@ -67,54 +48,11 @@ class Service
      *
      * Laravel validation rules: https://laravel.com/docs/10.x/validation
      *
-     * @return collection
+     * @return array
      */
-    public static function setPackageConfig()
+    public static function setPackageConfig(): array
     {
-        return collect([
-            [
-                "key" => "database_limit",
-                "name" => "Database Limit",
-                "description" => "The total number of databases a user is allowed to create for this server on Pterodactyl Panel.",
-                "type" => "number",
-                "rules" => ['required'], // laravel validation rules
-            ],
-            [
-                "key" => "allocation_limit",
-                "name" => "Allocation Limit",
-                "description" => "The total number of allocations a user is allowed to create for this server Pterodactyl Panel.",
-                "type" => "number",
-                "rules" => ['required'],
-            ],
-            [
-                "key" => "backup_limit",
-                "name" => "Backup Limit",
-                "description" => "The total number of backups that can be created for this server Pterodactyl Panel.",
-                "type" => "number",
-                "rules" => ['required'],
-            ],
-            [
-                "key" => "cpu_limit",
-                "name" => "CPU Limit in %",
-                "description" => "If you do not want to limit CPU usage, set the value to0. To use a single thread set it to 100%, for 4 threads set to 400% etc",
-                "type" => "number",
-                "rules" => ['required'],
-            ],
-            [
-                "key" => "memory_limit",
-                "name" => "Memory Limit in MB",
-                "description" => "The maximum amount of memory allowed for this container. Setting this to 0 will allow unlimited memory in a container.",
-                "type" => "number",
-                "rules" => ['required'],
-            ],
-            [
-                "key" => "disk_limit",
-                "name" => "Disk Limit in MB",
-                "description" => "The maximum amount of memory allowed for this container. Setting this to 0 will allow unlimited memory in a container.",
-                "type" => "number",
-                "rules" => ['required'],
-            ],
-        ]);
+        return [];
     }
 
     /**
@@ -123,11 +61,21 @@ class Service
      *
      * Laravel validation rules: https://laravel.com/docs/10.x/validation
      *
-     * @return collection
+     * @return array
      */
-    public static function setCheckoutConfig()
+    public static function setCheckoutConfig(): array
     {
-        return collect([]);
+        return [];    
+    }
+
+    /**
+     * Define buttons shown at order management page
+     *
+     * @return array
+     */
+    public static function setServiceButtons(): array
+    {
+        return [];    
     }
 
     /**
